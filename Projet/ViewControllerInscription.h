75b978529c62ea6a68f692a614efedbb91c16786
Anenzoras <anenzoras@gmail.com>
2012-12-27 16:49:24 +0100

    test3

---
+++ Projet/ViewControllerInscription.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerInscription.h
+//  Projet
+//
+//  Created by marcelin on 27/12/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerInscription : UIViewController
+
+@end
---
+++ Projet/ViewControllerInscription.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerInscription.m
+//  Projet
+//
+//  Created by marcelin on 27/12/12.
+//
+//
+
+#import "ViewControllerInscription.h"
+
+@interface ViewControllerInscription ()
+
+@end
+
+@implementation ViewControllerInscription
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
