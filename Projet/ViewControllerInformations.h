5b4657bab0dd60a0858f956925a02a426ccdc78d
ZHANG Xiaowei <phycctv@gmail.com>
2013-01-19 18:34:55 +0100

    cool

--- Projet/ViewControllerInformations.h
+++ Projet/ViewControllerInformations.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewControllerInformations : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *textNom;
+@property (weak, nonatomic) IBOutlet UITextField *textPrenom;
+@property (weak, nonatomic) IBOutlet UITextField *textJour;
+@property (weak, nonatomic) IBOutlet UITextField *textMois;
+@property (weak, nonatomic) IBOutlet UITextField *textAnnee;
+@property (weak, nonatomic) IBOutlet UITextField *textTel;
+- (IBAction)next:(id)sender;
+- (IBAction)confirmation:(id)sender;
+- (IBAction)begin:(id)sender;
+- (IBAction)end:(id)sender;
 
 @end
--- Projet/ViewControllerInformations.m
+++ Projet/ViewControllerInformations.m
@@ -13,6 +13,12 @@
 @end
 
 @implementation ViewControllerInformations
+@synthesize textNom;
+@synthesize textPrenom;
+@synthesize textJour;
+@synthesize textMois;
+@synthesize textAnnee;
+@synthesize textTel;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -35,4 +41,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setTextNom:nil];
+    [self setTextPrenom:nil];
+    [self setTextJour:nil];
+    [self setTextMois:nil];
+    [self setTextAnnee:nil];
+    [self setTextTel:nil];
+    [super viewDidUnload];
+}
+- (IBAction)next:(id)sender {
+}
+
+- (IBAction)confirmation:(id)sender {
+}
+
+- (IBAction)begin:(id)sender {
+}
+
+- (IBAction)end:(id)sender {
+}
 @end
