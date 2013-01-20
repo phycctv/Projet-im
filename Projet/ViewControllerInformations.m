0208d77baf9ace5fc35a44dd105b224b7ef2932f
ZHANG Xiaowei <phycctv@gmail.com>
2013-01-20 18:43:28 +0100

    textField

--- Projet/ViewControllerInformations.m
+++ Projet/ViewControllerInformations.m
@@ -48,17 +48,53 @@
     [self setTextMois:nil];
     [self setTextAnnee:nil];
     [self setTextTel:nil];
+    //self.textMois.placeholder=@"mm";
     [super viewDidUnload];
 }
 - (IBAction)next:(id)sender {
+    if(sender==textNom)
+    {
+        [textPrenom becomeFirstResponder];
+    }
+    else if(sender==textPrenom)
+    {
+        [textJour becomeFirstResponder];
+    }
+    else if(sender==textJour)
+    {
+        [textMois becomeFirstResponder];
+    }
+    else if(sender==textMois)
+    {
+        [textAnnee becomeFirstResponder];
+    }
+    else if(sender==textAnnee)
+    {
+        [textTel becomeFirstResponder];
+    }
 }
 
 - (IBAction)confirmation:(id)sender {
+    [sender resignFirstResponder];
 }
 
 - (IBAction)begin:(id)sender {
+    if(sender==textNom || sender==textPrenom )
+    {
+        self.view.center=CGPointMake(self.view.center.x,170);
+    }
+    else if(sender==textJour || sender==textMois || sender==textAnnee)
+    {
+        [sender setText:@""];
+        self.view.center=CGPointMake(self.view.center.x,120);
+    }
+    else if(sender==textTel)
+    {
+        self.view.center=CGPointMake(self.view.center.x,80);
+    }
 }
 
 - (IBAction)end:(id)sender {
+        self.view.center=CGPointMake(self.view.center.x,230);
 }
 @end
