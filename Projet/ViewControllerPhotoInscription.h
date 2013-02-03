784177d30db696291d5273497ad0fa7302773d60
ZHANG Xiaowei <phycctv@gmail.com>
2013-02-03 15:27:48 +0100

    fait chier, code no probleme, probleme de serveur php, impossible de upload

--- Projet/ViewControllerPhotoInscription.h
+++ Projet/ViewControllerPhotoInscription.h
@@ -12,12 +12,14 @@
     UIImagePickerController *pick2;
 }
 @property (unsafe_unretained, nonatomic) IBOutlet UIButton *butPrendrePhoto;
+@property (retain, nonatomic) IBOutlet UIButton *upload;
 @property (retain, nonatomic) IBOutlet UIImageView *imgView;
 - (IBAction)down:(id)sender;
 - (IBAction)cancel:(id)sender;
 - (IBAction)end:(id)sender;
 - (IBAction)up:(id)sender;
 @property (retain, nonatomic) IBOutlet UIButton *choisir;
+- (IBAction)upload:(id)sender;
 
 
 @end
--- Projet/ViewControllerPhotoInscription.m
+++ Projet/ViewControllerPhotoInscription.m
@@ -15,6 +15,7 @@
 @implementation ViewControllerPhotoInscription
 @synthesize choisir;
 @synthesize butPrendrePhoto;
+@synthesize upload;
 @synthesize imgView;
 bool hasCamera;
 UIImage *image;
@@ -31,7 +32,7 @@ UIImage *image;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         hasCamera=YES;
     }else {
@@ -49,6 +50,7 @@ UIImage *image;
     [self setButPrendrePhoto:nil];
     [self setChoisir:nil];
     [self setImgView:nil];
+    [self setUpload:nil];
     [super viewDidUnload];
 }
 - (IBAction)down:(id)sender {
@@ -103,23 +105,119 @@ UIImage *image;
 - (void)dealloc {
     [choisir release];
     [imgView release];
+    [upload release];
     [super dealloc];
 }
-
+-(IBAction)upload:(id)sender{
+        NSData *imageData = UIImageJPEGRepresentation(imgView.image, 0.5);
+        NSString *urlString = @"http://projects.eyesnap.fr/project05/appli/upload.php";
+    
+        NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+        [request setURL:[NSURL URLWithString:urlString]];
+        [request setHTTPMethod:@"POST"];
+        
+        NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+        NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+        [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+        
+        NSMutableData *body = [NSMutableData data];
+        [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        NSString *temp = [NSString stringWithFormat:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"ipodfile.jpg\"\r\n"];
+        [body appendData:[[NSString stringWithString:temp] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[NSData dataWithData:imageData]];
+        [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [request setHTTPBody:body];
+        
+        NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"%@",returnString);
+        UIAlertView * view= [[UIAlertView alloc] initWithTitle:nil 
+                                                                                           message:NSLocalizedString(returnString, @"")  
+                                                                                          delegate:nil 
+                                                                                 cancelButtonTitle:NSLocalizedString(@"OK", @"")
+                                                                                 otherButtonTitles:nil   
+                                                                 ];
+    [view show];
+    [view release];    /*
+    //作為Round Rect Button的touch down事件
+    //3.在implementation區段實做uploadImage方法，上傳Image View中的圖片到server上。程式碼:
+    NSData *imageData = UIImageJPEGRepresentation(imgView.image, 90);
+    //取得要上傳的圖片
+    NSString *urlString = @"http://projects.eyesnap.fr/project05/appli/upload.php";
+    //設定網址
+    NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+    [request setURL:[NSURL URLWithString:urlString]];
+    [request setHTTPMethod:@"POST"];
+    //設定request物件
+    NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+    [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+    //加入header
+    NSMutableData *body = [NSMutableData data];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"ipodfile.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[NSData dataWithData:imageData]];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    //設定body
+    [request setHTTPBody:body];
+    
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    //上傳
+    NSLog(returnString);*/
+    UIStoryboard *board=[UIStoryboard storyboardWithName:@"MainStoryboard_iPhone" bundle:nil]; 
+    UITabBarController *nextViewController =[board instantiateViewControllerWithIdentifier:@"Connexion"];
+    //[self.navigationController pushViewController:nextViewController animated:YES];
+}
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
 
-		UIImage *originalImage;
+        UIImage *originalImage;
 
-			originalImage = [info objectForKey: UIImagePickerControllerEditedImage];
-			//imgView = [[UIImageView alloc] initWithImage:originalImage];
+            originalImage = [info objectForKey: UIImagePickerControllerEditedImage];
+            //imgView = [[UIImageView alloc] initWithImage:originalImage];
     [imgView setImage:originalImage];
     [self dismissModalViewControllerAnimated:YES];
-			//[imgView  ];
-		
+    
+    
+    
+    /*
+    NSData *imageData = UIImageJPEGRepresentation(originalImage, 90);
+    //取得要上傳的圖片
+    NSString *urlString = @"http://projects.eyesnap.fr/project05/appli/upload.php";
+    //設定網址
+    NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+    [request setURL:[NSURL URLWithString:urlString]];
+    [request setHTTPMethod:@"POST"];
+    //設定request物件
+    NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+    [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+    //加入header
+    NSMutableData *body = [NSMutableData data];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"ipodfile.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[NSData dataWithData:imageData]];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    //設定body
+    [request setHTTPBody:body];
+    
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+    //上傳
+    NSLog(returnString);
+     */
+
+            //[imgView  ];
+        
             
-	
+    
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
-	[pick2 dismissModalViewControllerAnimated:YES];
-}@end
+    [pick2 dismissModalViewControllerAnimated:YES];
+    }
+@end
